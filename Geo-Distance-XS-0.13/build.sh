4bda6036f896da2021b6840dff6986ff5544ed3f
melnika <annamelnik08@gmail.ru>
2017-02-11 23:25:37 +0100

    biuld.sh

---
+++ Geo-Distance-XS-0.13/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rm -rf /tmp/Geo-Distance-XS-0.13
+if [ $? != 0 ]; then
+	echo "Error rm"
+	exit 1
+else
+	echo "rm - ok"
+fi
+
+
+cp -r /media/sf_Share/Compare-C-vs-Perl/Geo-Distance-XS-0.13 /tmp
+if [ $? != 0 ]; then
+	echo "Error copy to tmp"
+	exit 1
+else
+	echo "Copy to tmp - ok"
+fi
+
+
+cd /tmp/Geo-Distance-XS-0.13
+if [ $? != 0 ]; then
+	echo "Error cd"
+	exit 1
+else
+	echo "cd - ok"
+fi
+
+
+perl ./Makefile.PL
+if [ $? != 0 ]; then
+	echo "Error Makefile.PL"
+	exit 1
+else
+	echo "Makefile.PL - ok"
+fi
+
+
+make 
+make test
